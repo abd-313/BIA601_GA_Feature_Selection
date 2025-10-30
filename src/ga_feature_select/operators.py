23346cfd0dbc23a53f9096e61f4bcc224f87f122
Abdulrahman Suleiman <belyamalkan.313@hotmail.com>
2025-10-30 11:17:46 -0400

    Feat: Correctly implemented GA core and fitness logic, restructuring code. (#6)
    Co-authored-by: jaltarala2-sudo <jaltarala2@gmail.com>

---
+++ src/ga_feature_select/operators.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+
+def crossover(parent1, parent2, crossover_rate):
+
+    # Implementation goes here
+    pass
+
+def mutation(chromosome, mutation_rate):
+    
+    # Implementation goes here
+    pass
